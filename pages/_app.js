085eb9e7349038471dc0cdbb0e7c34ceaff20e64
macklark <ganesh.kolavennu@gmail.com>
2021-09-14 19:56:41 +0530

    added global animations with framer-motion

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { motion } from "framer-motion";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
     <ThemeProvider attribute="class">
-      <Component {...pageProps} />
+      <motion.div
+        key={router.route}
+        initial={{ x: "-100vw" }}
+        animate={{ x: "0vw" }}
+        transition={{ duration: 0.5 }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
     </ThemeProvider>
   );
 }
