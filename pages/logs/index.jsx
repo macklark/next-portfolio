5d7d50d1b323820ed91c87965b716cd687f7b9f8
macklark <ganesh.kolavennu@gmail.com>
2021-10-07 09:55:11 +0530

    added a new log

--- pages/logs/index.jsx
+++ pages/logs/index.jsx
@@ -1,10 +1,54 @@
 import Navbar from "../../components/Navbar";
+import { createClient } from "contentful";
+import Image from "next/image";
+import Head from "next/head";
 
-export default function Logs() {
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const res = await client.getEntries({ content_type: "logs" });
+
+  return {
+    props: {
+      logs: res.items,
+    },
+    revalidate: 100,
+  };
+}
+
+export default function Logs({ logs }) {
   return (
-    <div>
+    <>
+      <Head>
+        <title>Ganesh Kolavennu | Logs</title>
+        <meta charset="UTF-8" />
+        <meta name="description" content="Ganesh portfolio" />
+        <meta name="author" content="macklark" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Navbar />
-      <h1>Logs</h1>
-    </div>
+      <div className="container mx-auto md:max-w-5xl mt-10 grid grid-cols-1 md:grid-cols-3">
+        {logs.map((log) => (
+          <div className="col-span-1 m-5 md:m-0" key={log.sys.id}>
+            <div className="bg-white rounded-lg shadow-lg dark:bg-gray-800">
+              <Image
+                src={"https:" + log.fields.thumbnail.fields.file.url}
+                width={log.fields.thumbnail.fields.file.details.image.width}
+                height={log.fields.thumbnail.fields.file.details.image.height}
+              />
+              <div className="p-4">
+                <h2 className="text-2xl font-bold">{log.fields.title}</h2>
+                <p className="text-gray-700 text-lg mt-5 dark:text-gray-100">
+                  {log.fields.description}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
