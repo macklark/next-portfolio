ad65b518a4a4e494a4bc9a07d1d2f46da4a70391
macklark <ganesh.kolavennu@gmail.com>
2021-10-09 19:54:52 +0530

    added @contentful/rich-text-types

--- pages/logs/[slug].jsx
+++ pages/logs/[slug].jsx
@@ -1,7 +1,8 @@
 import { createClient } from "contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { BLOCKS } from "@contentful/rich-text-types";
+import { BLOCKS, MARKS } from "@contentful/rich-text-types";
 import Head from "next/head";
+// import Image from "next/image";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -38,6 +39,33 @@ export async function getStaticProps({ params }) {
   };
 }
 
+const richTextOptions = {
+  renderNode: {
+    [BLOCKS.HEADING_2]: (node, children) => {
+      return <h2 className="text-4xl font-bold mb-10">{children}</h2>;
+    },
+    [BLOCKS.HEADING_3]: (node, children) => {
+      return <h3 className="text-3xl font-medium mb-5">{children}</h3>;
+    },
+    [BLOCKS.PARAGRAPH]: (node, children) => {
+      if (node.content[0].marks[0] === undefined) {
+        return <p className="text-lg mb-10">{children}</p>;
+      }
+
+      if (node.content[0].marks[0].type === "code") {
+        return (
+          <pre className="py-5 px-4 mb-5 bg-gray-200 dark:bg-gray-800 rounded border-l-8">
+            {children}
+          </pre>
+        );
+      }
+    },
+    [BLOCKS.UL_LIST]: (node, children) => {
+      return <ul className="list-disc pl-5">{children}</ul>;
+    },
+  },
+};
+
 export default function LogsPage({ log }) {
   return (
     <div className="container mx-auto max-w-4xl">
@@ -52,7 +80,9 @@ export default function LogsPage({ log }) {
         {log.fields.title}
       </p>
       <div className="mt-10 mx-5">
-        <div>{documentToReactComponents(log.fields.matter)}</div>
+        <div>
+          {documentToReactComponents(log.fields.matter, richTextOptions)}
+        </div>
       </div>
     </div>
   );
