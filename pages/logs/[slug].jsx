2d1a01223e564fcc44298e330fbfd23b9efe56fd
macklark <ganesh.kolavennu@gmail.com>
2021-10-11 10:09:41 +0530

    removed rich text options

--- pages/logs/[slug].jsx
+++ pages/logs/[slug].jsx
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { BLOCKS } from "@contentful/rich-text-types";
+// import { BLOCKS } from "@contentful/rich-text-types";
 import Head from "next/head";
 
 const client = createClient({
@@ -38,45 +38,45 @@ export async function getStaticProps({ params }) {
   };
 }
 
-const richTextOptions = {
-  renderNode: {
-    [BLOCKS.HEADING_2]: (node, children) => {
-      console.log(node.content[0].nodeType);
-      return (
-        <h2 className="text-4xl font-bold mb-10" id={node.content[0].nodeType}>
-          {children}
-        </h2>
-      );
-    },
-    [BLOCKS.HEADING_3]: (node, children) => {
-      return (
-        <h3 className="text-3xl font-medium mb-5" id={node.content[0].nodeType}>
-          {children}
-        </h3>
-      );
-    },
-    [BLOCKS.PARAGRAPH]: (node, children) => {
-      if (node.content[0].marks[0] === undefined) {
-        return <p className="text-lg mb-10">{children}</p>;
-      }
+// const richTextOptions = {
+//   renderNode: {
+//     [BLOCKS.HEADING_2]: (node, children) => {
+//       console.log(node.content[0].nodeType);
+//       return (
+//         <h2 className="text-4xl font-bold mb-10" id={node.content[0].nodeType}>
+//           {children}
+//         </h2>
+//       );
+//     },
+//     [BLOCKS.HEADING_3]: (node, children) => {
+//       return (
+//         <h3 className="text-3xl font-medium mb-5" id={node.content[0].nodeType}>
+//           {children}
+//         </h3>
+//       );
+//     },
+//     [BLOCKS.PARAGRAPH]: (node, children) => {
+//       if (node.content[0].marks[0] === undefined) {
+//         return <p className="text-lg mb-10">{children}</p>;
+//       }
 
-      if (node.content[0].marks[0].type === "code") {
-        return (
-          <pre className="py-5 px-4 mb-5 bg-gray-200 dark:bg-gray-800 rounded border-l-8">
-            {children}
-          </pre>
-        );
-      }
-    },
-    [BLOCKS.UL_LIST]: (node, children) => {
-      return (
-        <ul className="list-disc pl-5" id={node.content[0].nodeType}>
-          {children}
-        </ul>
-      );
-    },
-  },
-};
+//       if (node.content[0].marks[0].type === "code") {
+//         return (
+//           <pre className="py-5 px-4 mb-5 bg-gray-200 dark:bg-gray-800 rounded border-l-8">
+//             {children}
+//           </pre>
+//         );
+//       }
+//     },
+//     [BLOCKS.UL_LIST]: (node, children) => {
+//       return (
+//         <ul className="list-disc pl-5" id={node.content[0].nodeType}>
+//           {children}
+//         </ul>
+//       );
+//     },
+//   },
+// };
 
 function LogsPage({ log }) {
   return (
@@ -92,7 +92,7 @@ function LogsPage({ log }) {
         {log.fields.title}
       </p>
       <div className="mt-10 mx-5">
-        {documentToReactComponents(log.fields.matter, richTextOptions)}
+        {documentToReactComponents(log.fields.matter)}
       </div>
     </div>
   );
